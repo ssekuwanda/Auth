79ffaa2905e7fcb8ada3f80ad083d48bfca2b5c3
Ssekuwanda <cytixdoug@gmail.com>
2022-03-11 05:11:28 +0300

    editted urls

--- auth/users/urls.py
+++ auth/users/urls.py
@@ -4,8 +4,8 @@ from knox import views as knox_views
 
 urlpatterns = [
     # path('api/auth/', include('knox.urls')),
-    path('api/auth/register', SignUpAPI.as_view()),
-    path('api/auth/login', SignInAPI.as_view()),
-    path('api/auth/user', MainUser.as_view()),
-    path('api/auth/logout',knox_views.LogoutView.as_view(), name="knox-logout"),
+    path('api/register', SignUpAPI.as_view()),
+    path('api/login', SignInAPI.as_view()),
+    path('api/user', MainUser.as_view()),
+    path('api/logout',knox_views.LogoutView.as_view(), name="knox-logout"),
 ]
