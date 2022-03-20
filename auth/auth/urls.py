ce36ae973e063690697714f87aa664eb2084857f
Ssekuwanda <cytixdoug@gmail.com>
2022-03-20 23:34:54 +0300

    urls correction

--- auth/auth/urls.py
+++ auth/auth/urls.py
@@ -7,7 +7,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="AUTHENTICATION",
+      title="AUTHENTICATION TRAIL APP",
       default_version='v1',
       description="Test description",
       terms_of_service="https://www.google.com/policies/terms/",
